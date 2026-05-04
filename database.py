e36d4fa4d46c70665a6adc2c3198c9e872445c86
Kazakova-dev <kazakovasitora2@gmail.com>
2026-05-04 08:33:47 +0300

    xatolar togirlandi

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm  import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
@@ -10,17 +10,17 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # Engine -databasega ulanish
 engine = create_engine(DATABASE_URL)
 
-#session har bir so`rov uchun
-SessionLocal = sessionmaker(autocommmit= False, autoflush=False, bind=engine)
+# session har bir so`rov uchun
+SessionLocal = sessionmaker(autocommmit=False, autoflush=False, bind=engine)
 
-#Base barcha modellar shundan meros oladi
+# Base barcha modellar shundan meros oladi
 Base = declarative_base()
 
-#dependency har bir endpoint uchun sessiya beradi,ilatilgandan keyin yopadi
+
+# dependency har bir endpoint uchun sessiya beradi,ilatilgandan keyin yopadi
 def get_db():
-    db= SessionLocal()
+    db = SessionLocal()
     try:
         yield db
-    finally: 
+    finally:
         db.close()
-        
